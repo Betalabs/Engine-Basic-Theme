0f88e209680ca0c59bc8f0bb48e4068fe24e4353
Victor Macedo <victor.macedo@betalabs.com.br>
2020-07-30 13:12:20 -0300

    Mocking first component

---
+++ resources/views/mockings/menu/1.blade.php
@@ -0,0 +1,4 @@
+@extends('base')
+@section('content')
+    This is my first component
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('base');
 });
+
+Route::get('/components/menu/1', function () {
+    return view('mockings.menu.1');
+});
